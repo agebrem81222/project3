8b5f0368a5b0a988bf09e37b1e3fdda17b1b9ea2
alema <alema@10.0.0.114>
2023-04-06 00:19:21 -0400

    Initial commit

--- src/demo/Test.java
+++ src/demo/Test.java
@@ -5,7 +5,6 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("Test........");
 		System.out.println("Git Hub");
-		System.out.println(" kinfe");
 		System.out.println("Gebremichael");
 	}
 
